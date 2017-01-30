66bd6c1536549db757d32cad878d3786b6d221c9
CD4L <CD4L@users.noreply.github.com>
2017-01-30 11:44:46 -0400

    Hello from set up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -15,7 +15,7 @@ void ofApp::draw(){
 ofBackground(0);
 ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
 
-for(int i = 0; i < 3; i++)
+for(int i = 0; i < 20; i++)
  {
    ofRotate(20);
    ofDrawLine(0,0,0,220);
